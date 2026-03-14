8c5270927b64811af733c70bd5a43b55d306b0ca
Manish12588 <manish1561988@gmail.com>
2026-03-14 21:34:07 +0100

    Update package.json file

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,6 +6,26 @@ export default [
   {
     files: ["**/*.js", "**/*.jsx"],
     plugins: { react },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        }
+      },
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        window: "readonly",
+        document: "readonly",
+      }
+    },
+    settings: {
+      react: {
+        version: "detect",
+      }
+    },
     rules: {
       "no-unused-vars": "warn",
       "react/prop-types": "warn",
